dcf81e53d2f83d7580b0558dd9b9f1d77dc169c5
Diane Chen <dianedc@purpleweb.com>
2022-10-23 16:25:34 -0700

    Add favicon display; repair models.py

--- polls/models.py
+++ polls/models.py
@@ -8,7 +8,6 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    search_fields = ['question_text']
 
     def __str__(self):
         return self.question_text
